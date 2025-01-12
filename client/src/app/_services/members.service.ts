1107fdf9d9fe29c2e77d8d4f9220aa636e4c2777
Danahalquaamiz <alquaamizdanah@gmail.com>
2025-01-12 14:05:20 +0300

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { AccountService } from './account.service';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/Photo';
 
 @Injectable({
   providedIn: 'root'
@@ -23,14 +24,40 @@ export class MembersService {
 
   getMember(username: string) {
     const member = this.members().find(x => x.userName === username);
-    if(member !== undefined) return of(member);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(member:Member){
+  updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       tap(() => {
         this.members.update(members => members.map(m => m.userName == member.userName ? member : m))
+      })
+    )
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photoUrl = photo.url
+          }
+          return m;
+        }))
+      })
+    )
+  }
+
+  deletePhoto(photo: Photo) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if (m.photos.includes(photo)) {
+            m.photos = m.photos.filter(x => x.id !== photo.id)
+          }
+          return m;
+        }))
       })
     )
   }
